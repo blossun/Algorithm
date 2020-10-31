6df025285730bc72b6aeaefe6f304e377d026a61
blossun <dnsngu@gmail.com>
2020-10-31 21:22:47 +0900

    [BJ] 1926 그림 _ java 코드 수정
    좀 더 깔끔하게 수정
    
    issue #51

--- baekjoon/java/src/main/java/dev/solar/baekjoon/N1926.java
+++ baekjoon/java/src/main/java/dev/solar/baekjoon/N1926.java
@@ -1,5 +1,7 @@
 package dev.solar.baekjoon;
 
+import com.sun.tools.javac.util.Pair;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,91 +10,71 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class N1926 {
+    static int[][] board; // 칸
+    static boolean[][] visit; // 방문 여부 표시 : 1 - 방문함, 0 - 방문 안 함
+    static int[] dx = {1, 0, -1, 0}; //상하좌우 네 방향
+    static int[] dy = {0, 1, 0, -1};
+    static int N;
+    static int M;
+
+    static class Point {
+        int x;
+        int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[][] board = new int[n][m]; // 칸
-        int[][] visit = new int[n][m]; // 방문 여부 표시 : 1 - 방문함, 0 - 방문 안 함
-        int[] dx = {1, 0, -1, 0}; //상하좌우 네 방향
-        int[] dy = {0, 1, 0, -1};
-        for (int i = 0; i < n; i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine().trim(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        board = new int[N][M];
+        visit = new boolean[N][M];
+        int maxWidth = 0; //그림 최댓값
+        int count = 0; //그림의 수
+
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine()); //한 줄씩 받아줘야 함
-            for (int j = 0; j < m; j++) {
+            for (int j = 0; j < M; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int max = 0; //그림 최댓값
-        int num = 0; //그림의 수
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) { //(i, j)를 시작지점으로
-                //해당 좌표가 그림부분이 아니거나(0) 이미 방문한 곳(visit[i][j]이면 넘어감
-                if (board[i][j] == 0 || visit[i][j] != 0) continue;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) { //(i, j)를 시작지점으로
+                //해당 좌표가 그림부분이 아니거나(0) 이미 방문한 곳(visit[i][j] == ture_이면 넘어감
+                if (board[i][j] == 0 || visit[i][j]) continue;
                 // (i, j)는 새로운 그림에 속해있는 시작점
-                num++; // 그림의 수 1 증가
-                Queue<Pair<Integer, Integer>> q = new LinkedList<>(); //큐에 (x,y)좌표 pair 타입 데이터 저장
+                count++; // 그림의 수 1 증가
+                Queue<Point> q = new LinkedList<>(); //큐에 (x,y)좌표 pair 타입 데이터 저장
                 // (i, j)를 BFS의 시작점으로 출발하기 위한 준비
                 // 방문 표시를 큐에 넣을 때 해줌!!!!
                 // (뺄 때 표시 no -> 같은 칸이 큐에 여러 번 들어가서 시간 초과나 메모리 초과 발생할 수 있음)
-                visit[i][j] = 1; // 시작점을 방문했다고 표시
-                q.add(new Pair<>(i, j));
+                visit[i][j] = true; // 시작점을 방문했다고 표시
+                q.add(new Point(i, j));
                 int area = 0; //현재 그림의 넒이
-                while(!q.isEmpty()) {
+                while (!q.isEmpty()) {
                     area++; //큐에 들어있는 원소를 하나 뺄 때 마다 넓이를 1 증가시킴
-                    Pair cur = q.poll();
+                    Point cur = q.poll();
                     for (int dir = 0; dir < 4; dir++) { //현재 좌표의 상하좌우 칸을 살펴봄
-                        int nx = (int) cur.getLeft() + dx[dir];
-                        int ny = (int) cur.getRight() + dy[dir];
-                        if (nx < 0 || nx >= n || ny < 0 || ny >= m ) continue; // 범위 밖일 경우 넘어감
-                        if(visit[nx][ny] != 0 || board[nx][ny] != 1) continue; // 이미 방문한 칸이거나 파란 칸이 아닐 경우
-                        visit[nx][ny] = 1; //(nx, ny)를 방문했다고 명시
-                        q.add(new Pair<>(nx, ny));
+                        int nx = cur.x + dx[dir];
+                        int ny = cur.y + dy[dir];
+                        if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue; // 범위 밖일 경우 넘어감
+                        if (visit[nx][ny] || board[nx][ny] != 1) continue; // 이미 방문한 칸이거나 파란 칸이 아닐 경우
+                        visit[nx][ny] = true; //(nx, ny)를 방문했다고 명시
+                        q.add(new Point(nx, ny));
                     }
                 }
-                max = Math.max(max, area); // area가 mx보다 클 경우 mx에 area를 대입.
+                maxWidth = Math.max(maxWidth, area); // area가 mx보다 클 경우 mx에 area를 대입.
             }
         }
-        System.out.println(num + "\n" + max);
-    }
-
-//    static class BFS {
-//        int x, y; // 좌표값 (행, 열)
-////        int n, m; // n : 행 크기, m : 열 크
-//        int[][] board; //보드 - 1 : 그림, 0 : no
-//        int[][] visit;
-//
-//        public BFS(int[][] board, int n, int m) {
-//            this.board = board;
-//            this.visit = new int[n][m];
-//        }
-//    }
-
-    public static class Pair<L,R> {
-
-        private final L left;
-        private final R right;
-
-        public Pair(L left, R right) {
-            this.left = left;
-            this.right = right;
-        }
-
-        public L getLeft() { return left; }
-        public R getRight() { return right; }
-
-        @Override
-        public int hashCode() { return left.hashCode() ^ right.hashCode(); }
-
-        @Override
-        public boolean equals(Object o) {
-            if (!(o instanceof Pair)) return false;
-            Pair pairo = (Pair) o;
-            return this.left.equals(pairo.getLeft()) &&
-                    this.right.equals(pairo.getRight());
-        }
-
+        System.out.println(count);
+        System.out.println(maxWidth);
     }
 }
 
