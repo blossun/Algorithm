eca98cea52c367210aaba1609262f2ff8d518e7b
blossun <dnsngu@gmail.com>
2020-11-11 00:35:20 +0900

    [BJ] 5904 Moo게임 - 분할정복 다시 풀기
    issue #70

--- baekjoon/java/src/main/java/dev/solar/baekjoon/N5904.java
+++ baekjoon/java/src/main/java/dev/solar/baekjoon/N5904.java
@@ -1,43 +1,38 @@
 package dev.solar.baekjoon;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class N5904 {
-    static int N;
-    static List<Character> mooStr;
+    static int N; //출력하고자 하는 인덱스
+    static int[] MooLen = new int[100]; //Moo의 길이를 저장
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        mooStr = new ArrayList<>();
         N = sc.nextInt();
-        moo(N);//수열을 만들기만?
-        System.out.println(mooStr.get(N - 1));
-        System.out.println(mooStr.toString());
+        MooLen[0] = 3;
+        for (int i = 1; i < 100; i++) {
+            MooLen[i] = MooLen[i - 1] * 2 + (i + 3);
+            if (MooLen[i] > 1e9) break;
+        }
+
+        System.out.println(moo(N));
     }
 
-    private static void moo(int n) {
-        if (mooStr.size() >= N) //구하려는 N번쨰 이후 값은 필요 없음
-        {
-            System.out.println("size : " + mooStr.size());
-            return;
+    private static char moo(int n) {
+        if (n <= 3) { //baseCondition
+            if (n == 1) return 'm';
+            else return 'o';
         }
 
-        if (n == 0) {
-            mooStr.add('m');
-            mooStr.add('o');
-            mooStr.add('o');
-            return;
+        int m = 1;
+        while (n > MooLen[m]) //MooLen을 통해서 어느 S(M)에 속하는지 찾는다.
+            m++;
+
+        if (n <= MooLen[m] - MooLen[m - 1]) { //Moo..oo 구간에 속한 경우
+            if (n - MooLen[m - 1] == 1) return 'm'; //첫번째만 'm' 출력
+            else return 'o'; //이후에 'n'출력
         }
 
-        moo(n - 1);
-        mooStr.add('m');
-        for (int i = 0; i < n + 2; i++) {
-            if (mooStr.size() >= N) //구하려는 N번쨰 이후 값은 필요 없음
-                return;
-            mooStr.add('o');
-        }
-        moo(n - 1);
+        return moo(n - MooLen[m - 1] - (m + 3)); //S(N-1)구간에 (앞,뒤) 존재하는 경우
     }
 }
