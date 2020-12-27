bea6ac00fc913fdcb05da003ef968c2b4f6aa2ce
blossun <dnsngu@gmail.com>
2020-12-27 18:21:28 +0900

    [BJ] 2217 로프
    closed #115

---
+++ baekjoon/java/src/main/java/dev/solar/baekjoon/N2217.java
@@ -0,0 +1,24 @@
+package dev.solar.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class N2217 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        int k = N;
+        for (int i = 0; i < N; i++) {
+            max = Math.max(max, arr[i] * k--);
+        }
+        System.out.println(max);
+    }
+}
