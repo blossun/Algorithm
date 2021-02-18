765975a910f9128e293100ff739020058c42ef3d
blossun <dnsngu@gmail.com>
2021-02-19 00:38:27 +0900

    [BJ] 1026 보물
    Closed #142

---
+++ baekjoon/java/src/main/java/dev/solar/baekjoon/N1026.java
@@ -0,0 +1,34 @@
+package dev.solar.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N1026 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arrA = new int[N];
+        int[] arrB = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arrA[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arrB[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arrB);
+        Arrays.sort(arrA);
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += arrA[i] * arrB[N - 1 - i];
+        }
+        System.out.println(sum);
+    }
+}
