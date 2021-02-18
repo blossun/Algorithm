30c3ef9f6c5712fcb398fedc2b4cc3a9d259a536
blossun <dnsngu@gmail.com>
2021-02-18 22:53:48 +0900

    [BJ] 11652 카드
    Closed #140

--- baekjoon/java/src/main/java/dev/solar/baekjoon/N11652.java
+++ baekjoon/java/src/main/java/dev/solar/baekjoon/N11652.java
@@ -3,63 +3,38 @@ package dev.solar.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
 
 public class N11652 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        List<Node> nodes = new LinkedList<>();
+        long[] arr = new long[N];
 
-        while (N-- > 0) {
-            long number = Long.parseLong(br.readLine());
-            Node newNode = new Node(number);
-            int index = nodes.indexOf(newNode);
-            if (index != -1) {
-                nodes.get(index).plus();
+        for (int i = 0; i < N; i++) {
+            arr[i] = Long.parseLong(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int maxCnt = 1; //0으로 잘못 카운트 했음
+        int cnt = 1;
+        long maxCntNumber = arr[0];
+
+        for (int i = 1; i < N; i++) {
+            if (arr[i - 1] != arr[i]) {
+                cnt = 1;
             } else {
-                nodes.add(newNode);
+                cnt++;
+            }
+            if (maxCnt < cnt) {
+                maxCnt = cnt;
+                maxCntNumber = arr[i];
             }
         }
 
-        Collections.sort(nodes);
-        System.out.println(nodes.get(0).number);
-    }
+        System.out.println(maxCntNumber);
 
-    private static class Node implements Comparable<Node>{
-        public Long number;
-        public int count;
-
-        public Node(long number) {
-            this.number = number;
-            this.count = 0;
-        }
-
-        public void plus() {
-            count++;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof Node)) return false;
-            Node node = (Node) o;
-            return Objects.equals(number, node.number);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(number);
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            if (this.count != o.count) {
-                return o.count - this.count;
-            } else {
-                return this.number.compareTo(o.number);
-            }
-        }
     }
 }
