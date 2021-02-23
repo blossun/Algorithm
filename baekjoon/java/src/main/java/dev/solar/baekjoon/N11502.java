34a86cea27dde6e2e35a9055a3cbc431b8022157
blossun <dnsngu@gmail.com>
2021-02-23 15:10:11 +0900

    [BJ] 11501 주식
    Closed #149

--- baekjoon/java/src/main/java/dev/solar/baekjoon/N11502.java
+++ baekjoon/java/src/main/java/dev/solar/baekjoon/N11502.java
@@ -6,9 +6,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class N11502 {
-    private static Node top;
-    private static int count;
-    private static int sumOfChild;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,84 +13,23 @@ public class N11502 {
         int T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
             int N = Integer.parseInt(br.readLine());
+            int[] arr = new int[N];
+
             st = new StringTokenizer(br.readLine());
-            top = new Node(Integer.parseInt(st.nextToken())); //맨 처음 노드
-            while (--N > 0) { // 맨 처음 입력받은 횟수 차감 후 시작
-                Node cur = new Node(Integer.parseInt(st.nextToken()));
-                if (cur.value >= top.value) {
-                    cur.setLeft(top);
-                    top = cur;
-                } else {
-                    if (top.getRight() == null) {
-                        top.setRight(cur);
-                    } else {
-                        Node parentNode = top;
-                        while (parentNode.getRight() != null) {
-                            if (cur.value >= parentNode.getRight().value) {
-                                cur.setLeft(parentNode.getRight());
-                                break;
-                            } else {
-                                parentNode = parentNode.getRight();
-                            }
-                        }
-                        parentNode.setRight(cur);
-                    }
-                }
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            int total = 0;
-            while (top != null && top.getLeft() != null) {
-                count = 0;
-                sumOfChild = 0;
-                preorder(top.getLeft());
-                total += top.value * count - sumOfChild;
-                top = top.getRight();
+            long total = 0;
+            int max = arr[N - 1];
+            for (int i = N - 2; i >= 0; i--) {
+                if (arr[i] < max) {
+                    total += max - arr[i];
+                } else {
+                    max = arr[i];
+                }
             }
             System.out.println(total);
         }
-    }
-
-    private static void preorder(Node top) {
-        if (top == null) {
-            return;
-        }
-
-        count++;
-        sumOfChild += top.value;
-        preorder(top.getLeft());
-        preorder(top.getRight());
-    }
-
-    public static class Node {
-        public int value;
-        public Node left;
-        public Node right;
-
-        public Node(int value) {
-            this.value = value;
-        }
-
-        public Node(int value, Node left, Node right) {
-            this.value = value;
-            this.left = left;
-            this.right = right;
-        }
-
-        public Node getLeft() {
-            return left;
-        }
-
-        public Node getRight() {
-            return right;
-        }
-
-        public void setLeft(Node left) {
-            this.left = left;
-        }
-
-        public void setRight(Node right) {
-            this.right = right;
-        }
-
     }
 }
