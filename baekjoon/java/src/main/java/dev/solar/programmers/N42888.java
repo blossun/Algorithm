a21862ca567eeaa54f9224cc386b1a3bf54cf547
blossun <dnsngu@gmail.com>
2021-06-30 18:23:15 +0900

    [Programmers] 42888 오픈채팅방
    Closed #246

--- baekjoon/java/src/main/java/dev/solar/programmers/N42888.java
+++ baekjoon/java/src/main/java/dev/solar/programmers/N42888.java
@@ -5,36 +5,34 @@ import java.util.LinkedList;
 
 public class N42888 {
     public String[] solution(String[] record) {
-        // O(n^2) 시간 초과
         HashMap<String, String> nickName = new HashMap<>();
-        LinkedList<String[]> logging = new LinkedList<>();
+        LinkedList<String> answer = new LinkedList<>();
         for (String str : record) {
             String[] words = str.split(" ");
             String command = words[0];
+            // 최종 닉네임만 먼저 업데이트 해놓는다.
             switch (command) {
                 case "Enter":
                     nickName.put(words[1], words[2]);
-                    logging.add(new String[]{"Enter", words[1]});
-                    break;
-                case "Leave":
-                    logging.add(new String[]{"Leave", words[1]});
                     break;
                 case "Change":
                     nickName.put(words[1], words[2]);
             }
         }
-        String[] answer = new String[logging.size()];
-        for (int i = 0; i < logging.size(); i++) {
-            String[] line = logging.get(i);
-            StringBuilder sb = new StringBuilder();
-            sb.append(nickName.get(line[1])).append("님이 ");
-            if (line[0] == "Enter") {
-                sb.append("들어왔습니다.");
-            } else {
-                sb.append("나갔습니다.");
+        for (String str : record) {
+            String[] words = str.split(" ");
+            String command = words[0];
+            String uid = words[1];
+            // 로그 기록
+            switch (command) {
+                case "Enter":
+                    answer.add(nickName.get(uid) + "님이 들어왔습니다.");
+                    break;
+                case "Leave":
+                    answer.add(nickName.get(uid) + "님이 나갔습니다.");
             }
-            answer[i] = sb.toString();
+
         }
-        return answer;
+        return answer.toArray(new String[0]);
     }
 }
--- baekjoon/java/src/test/java/dev/solar/programmers/N42888Test.java
+++ baekjoon/java/src/test/java/dev/solar/programmers/N42888Test.java
@@ -12,6 +12,6 @@ class N42888Test {
         String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
         String[] expected = {"Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."};
         String[] actual = n42888.solution(record);
-        assertArrayEquals(expected, record);
+        assertArrayEquals(expected, actual);
     }
 }
