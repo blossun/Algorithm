3dadab1252b8a4e2c1a6e3e6e4e1c68a53e17de4
blossun <dnsngu@gmail.com>
2021-07-01 21:01:36 +0900

    [Programmers] 42885 구명보드
    Closed #241

--- baekjoon/java/src/main/java/dev/solar/programmers/N42885.java
+++ baekjoon/java/src/main/java/dev/solar/programmers/N42885.java
@@ -6,7 +6,43 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class N42885 {
+
     public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int min = 0;
+        int count = 0;
+        for (int max = people.length - 1; min <= max; max--) {
+            if (people[min] + people[max] <= limit) min++;
+            count++;
+        }
+        return count;
+    }
+
+    public int solution02(int[] people, int limit) { //효율성실패
+        Arrays.sort(people);
+        int count = 0;
+        for (int i = 0; i < people.length; i++) {
+            int currentWeight = people[i];
+            if (currentWeight == -1) {
+                continue;
+            }
+            people[i] = -1;
+            count++;
+            for (int j = people.length - 1; j > i ; j--) {
+                if (people[j] == -1) {
+                    continue;
+                }
+                if (currentWeight + people[j] > limit) {
+                    continue;
+                }
+                currentWeight += people[j];
+                people[j] = -1;
+            }
+        }
+        return count;
+    }
+
+    public int solution01(int[] people, int limit) {// 효율성 실패
         List<Integer> peopleList = Arrays.stream(people).boxed().collect(Collectors.toList());
         Collections.sort(peopleList);
         int count = 0;
