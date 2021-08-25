f780d9e7fcdb28d5dc4671ac23adb0d5f776d1e7
blossun <blossunn@gmail.com>
2021-08-25 16:20:31 +0900

    [BJ] 10815 숫자 카드
    issue #285

---
+++ baekjoon/java/src/main/java/dev/solar/baekjoon/N10815.java
@@ -0,0 +1,35 @@
+package dev.solar.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N10815 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+        int[] answer = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int result = Arrays.binarySearch(arr, Integer.parseInt(st.nextToken()));
+            if (result >= 0) {
+                answer[i] = 1;
+            }
+        }
+
+        for (int n : answer) {
+            System.out.print(n + " ");
+        }
+    }
+}
