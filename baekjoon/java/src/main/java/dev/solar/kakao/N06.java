d6281eeb6dbeee4f8fc05a2ec4a9b82d692ea152
blossun <blossunn@gmail.com>
2021-09-11 16:53:09 +0900

    [kakao] n06
    issue $170

--- baekjoon/java/src/main/java/dev/solar/kakao/N06.java
+++ baekjoon/java/src/main/java/dev/solar/kakao/N06.java
@@ -7,15 +7,23 @@ import java.util.stream.Stream;
 
 public class N06 {
     public int solution(int[][] board, int[][] skills) {
+        int answer = board.length * board[0].length;
         for (int[] skill : skills) {
             int value = skill[0] == 1 ? skill[5] * -1 : skill[5];
             for (int i = skill[1]; i <= skill[3]; i++) {
                 for (int j = skill[2]; j <= skill[4]; j++) {
-                    board[i][j] += value;
+                    int tmp = board[i][j];
+                    int newValue = tmp + value;
+                    if (tmp <= 0 && newValue > 0) { //음수 -> 양수
+                        answer++;
+                    }
+                    if (tmp > 0 && newValue <= 0) {
+                        answer--;
+                    }
+                    board[i][j] = newValue;
                 }
             }
         }
-        int answer = 0;
 //        for (int[] line : board) {
 //            for (int i : line) {
 //                if (i > 0) {
@@ -23,10 +31,11 @@ public class N06 {
 //                }
 //            }
 //        }
-        answer = (int) Arrays.asList(board).parallelStream()
-                .flatMapToInt(arrays -> Arrays.stream(arrays))
-                .filter(data -> data > 0)
-                .count();
+
+//        answer = (int) Arrays.asList(board).parallelStream()
+//                .flatMapToInt(arrays -> Arrays.stream(arrays))
+//                .filter(data -> data > 0)
+//                .count();
         return answer;
     }
 }
